2a71216bd93ede8c99e4d048b770843a217f3835
shameCanales <canalesshame@gmail.com>
2025-04-30 17:51:04 +0800

    Cleaned up the code for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -10,43 +10,11 @@ function App() {
   const teamData = useSelector((state) => state.score);
   console.log(teamData.guest.name);
 
-  // const [teamData, setTeamData] = useState({
-  //   home: {
-  //     name: "home",
-  //     // score: 0,
-  //     score: 0,
-  //   },
-  //   guest: {
-  //     name: "guest",
-  //     score: 3,
-  //   },
-  // });
-
-  // function handleClick(team, point) {
-  //   setTeamData((prevData) => {
-  //     return {
-  //       ...prevData,
-  //       [team]: {
-  //         ...prevData[team],
-  //         score: newScore,
-  //       }, //how? 1. copy the previous data 2. modify the score of the team 3. return the new object then react will detect the change and rerender the component.
-  //     };
-  //   });
-  // }
 
   function handleClear() {
     dispatch(scoreActions.reset());
   }
 
-  // const editableTeamName = (
-  //   <input
-  //     type="text"
-  //     name="teamName"
-  //     className="teamName"
-  //     value={teamData.home.name}
-  //     required
-  //   />
-  // );
 
   return (
     <div className="p-6 m-6 rounded-2xl text-center text-neutral-50 bg-gradient-to-tl from-neutral-900 to-neutral-700  ">
